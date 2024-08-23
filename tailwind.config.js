30861fd9fb26fded2a30bc43459ff49a16b8d937
Sagor Ahmed <ashrafniloy512@gmail.com>
2024-08-23 17:17:36 +0600

    error fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('/assets/bg.jpg')"
+        'hero-pattern': "url('assets/bg.jpg')"
       }
     },
   },
