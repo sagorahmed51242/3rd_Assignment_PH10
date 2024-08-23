d58dba451831a5e08e62e25c6b5fa92f6e61f919
Sagor Ahmed <ashrafniloy512@gmail.com>
2024-08-23 16:47:23 +0600

    discuss section done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/bg.jpg')"
+      }
+    },
   },
   plugins: [],
 }
